76f0af5ab9ef6f651da6311dd6fc7a6724bacaec
HINA KHADIM <hinakhadim2002@gmail.com>
2020-11-03 09:44:58 +0500

    add the resources page

---
+++ women-in-tech-website/githubRepo.js
@@ -0,0 +1,100 @@
+export const GithubRepos = [
+  {
+    name: "Complete DSA Study Plan",
+    link: "https://github.com/shikha-16/Women-in-Technology",
+    excerpt:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    username: "GITHUB USERNAME",
+  },
+  {
+    name: "Awesome Lists",
+    link: "https://github.com/shikha-16/Women-in-Technology",
+    excerpt:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    username: "GITHUB USERNAME",
+  },
+  {
+    name: "Interview Prep",
+    link: "https://github.com/shikha-16/Women-in-Technology",
+    excerpt:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    username: "GITHUB USERNAME",
+  },
+  {
+    name: "Open Source",
+    link: "https://github.com/shikha-16/Women-in-Technology",
+    excerpt:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    username: "GITHUB USERNAME",
+  },
+  {
+    name: "University Courses",
+    link: "https://github.com/shikha-16/Women-in-Technology",
+    excerpt:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    username: "GITHUB USERNAME",
+  },
+  {
+    name: "Python Algorithms",
+    link: "https://github.com/shikha-16/Women-in-Technology",
+    excerpt:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    username: "GITHUB USERNAME",
+  },
+  {
+    name: "First Open Source Contribution",
+    link: "https://github.com/shikha-16/Women-in-Technology",
+    excerpt:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    username: "GITHUB USERNAME",
+  },
+  {
+    name: "Github",
+    link: "https://github.com/shikha-16/Women-in-Technology",
+    excerpt:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    username: "GITHUB USERNAME",
+  },
+  {
+    name: "Research Internships for Undergrad Students",
+    link: "https://github.com/shikha-16/Women-in-Technology",
+    excerpt:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    username: "GITHUB USERNAME",
+  },
+  {
+    name: "Complete Guide to Computer Science",
+    link: "https://github.com/shikha-16/Women-in-Technology",
+    excerpt:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    username: "GITHUB USERNAME",
+  },
+  {
+    name: "Developer Roadmap",
+    link: "https://github.com/shikha-16/Women-in-Technology",
+    excerpt:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    username: "GITHUB USERNAME",
+  },
+  {
+    name: "Best websites for Programmers",
+    link: "https://github.com/shikha-16/Women-in-Technology",
+    excerpt:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    username: "GITHUB USERNAME",
+  },
+  {
+    name: "Competitive Coding Resources",
+    link: "https://github.com/shikha-16/Women-in-Technology",
+    excerpt:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    username: "GITHUB USERNAME",
+  },
+  {
+    name: "SDE Interview Questions",
+    link: "https://github.com/shikha-16/Women-in-Technology",
+    excerpt:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    username: "GITHUB USERNAME",
+  },
+];
