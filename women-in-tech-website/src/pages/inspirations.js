57bba12c7fc423277f9bf47b5209fbc613cca12d
Rishabh <62797354+prishison@users.noreply.github.com>
2020-11-20 14:57:44 +0530

    Inspirations Page for Women in Tech Website
    The page contains 
    -navigation bar
    -carousel component
    -footer

---
+++ women-in-tech-website/src/pages/inspirations.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from "../components/Header";
+import CarouselComponent from "../components/CarouselComponent";
+import ResourcesComponent from "../components/resourcesComponent/resourcesComponent";
+import Footer from "../components/Footer";
+
+export default function inspirations() {
+    return (
+        <div className="about">
+          <Header />
+          <div className="resources-page">
+            
+          </div>
+          <CarouselComponent />
+          <Footer />
+        </div>
+      );
+}
