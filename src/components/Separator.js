cc3a83fd8e5a652f11ee25117ae32bb109f7fbba
Neha Kalbande <neha.leenaharsh@gmail.com>
2021-06-05 12:28:01 +0530

    Delete src directory

--- src/components/Separator.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function Separator() {
-  return <div style={{ borderBottom: '4px solid' }} />;
-}
--- src/pages/404.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const PageNotFound = () => (
-  <>
-    <h1>Page not found</h1>
-  </>
-);
-export default PageNotFound;
