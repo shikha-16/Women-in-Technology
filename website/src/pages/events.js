586fe9c752e219251d390a84e8a6cbf3673d2016
Rishabh Khandelwal <rishabhk2512@gmail.com>
2021-06-09 17:08:58 +0530

    events page added

---
+++ website/src/pages/events.js
@@ -0,0 +1,37 @@
+import React from 'react';
+// import Header from '../components/Header/Header';
+import ResourcesComponent from '../components/resourcesComponent/resourcesComponent';
+// import Footer from '../components/Footer/Footer';
+
+
+
+export default function EventsPage() {
+  return (
+    <div className="about">
+     
+      <h1 style={{paddingTop:20}}> Events/Programs</h1>
+      <ul style={{listStyle:'none'}}>
+      <li><a href="">Google Codejam io for Women</a></li>
+      <li><a href="">Google STEP Internship</a></li>
+      <li><a href="">Girlscript Summer of Code</a></li>
+      <li><a href="">Google Summer of Code</a></li>
+      <li><a href="">She Codes by Indeed</a></li>
+      <li><a href="">Outreachy</a></li>
+      <li><a href="">Goldman Sachs Women Possibilities Summit: Finance</a></li>
+      <li><a href="">Tech Women at Intuit</a></li>
+      <li><a href="">Women Who Code: Delhi</a></li>
+      <li><a href="">Society of Women Engineers India</a></li>
+      <li><a href="">DevelopHER</a></li>
+      <li><a href="">LinkedIn Wintathon</a></li>
+      <li><a href="">Adobe Codiva</a></li>
+      <li><a href="">Visa Code Your Way</a></li>
+      <li><a href="">Microsoft Codess</a></li>
+      <li><a href="">Uber She++</a></li>
+      <li><a href="">Flipkart Girls Wanna Code</a></li>
+      <li><a href="">LinkedIn CoachIn Program</a></li>
+
+      </ul>
+ 
+    </div>
+  );
+}
