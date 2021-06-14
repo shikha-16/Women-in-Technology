b2ff30e13d55ac3064b32be70405454d158985d6
Rishabh Khandelwal <rishabhk2512@gmail.com>
2021-06-14 19:04:23 +0530

    Events page updated

--- website/src/pages/events.js
+++ website/src/pages/events.js
@@ -2,35 +2,36 @@ import React from 'react';
 // import Header from '../components/Header/Header';
 import ResourcesComponent from '../components/resourcesComponent/resourcesComponent';
 // import Footer from '../components/Footer/Footer';
+import './events.css'
 
 
 
 export default function EventsPage() {
   return (
-    <div className="about">
+    <div className="list">
      
       <h1 style={{paddingTop:20}}> Events/Programs</h1>
-      <ul style={{listStyle:'none'}}>
-      <li><a href="">Google Codejam io for Women</a></li>
-      <li><a href="">Google STEP Internship</a></li>
-      <li><a href="">Girlscript Summer of Code</a></li>
-      <li><a href="">Google Summer of Code</a></li>
-      <li><a href="">She Codes by Indeed</a></li>
-      <li><a href="">Outreachy</a></li>
-      <li><a href="">Goldman Sachs Women Possibilities Summit: Finance</a></li>
-      <li><a href="">Tech Women at Intuit</a></li>
-      <li><a href="">Women Who Code: Delhi</a></li>
-      <li><a href="">Society of Women Engineers India</a></li>
-      <li><a href="">DevelopHER</a></li>
-      <li><a href="">LinkedIn Wintathon</a></li>
-      <li><a href="">Adobe Codiva</a></li>
-      <li><a href="">Visa Code Your Way</a></li>
-      <li><a href="">Microsoft Codess</a></li>
-      <li><a href="">Uber She++</a></li>
-      <li><a href="">Flipkart Girls Wanna Code</a></li>
-      <li><a href="">LinkedIn CoachIn Program</a></li>
+      <ol>
+      <li><a href="#">Google Codejam io for Women</a></li>
+      <li><a href="#">Google STEP Internship</a></li>
+      <li><a href="#">Girlscript Summer of Code</a></li>
+      <li><a href="#">Google Summer of Code</a></li>
+      <li><a href="#">She Codes by Indeed</a></li>
+      <li><a href="#">Outreachy</a></li>
+      <li><a href="#">Goldman Sachs Women Possibilities Summit: Finance</a></li>
+      <li><a href="#">Tech Women at Intuit</a></li>
+      <li><a href="#">Women Who Code: Delhi</a></li>
+      <li><a href="#">Society of Women Engineers India</a></li>
+      <li><a href="#">DevelopHER</a></li>
+      <li><a href="#">LinkedIn Wintathon</a></li>
+      <li><a href="#">Adobe Codiva</a></li>
+      <li><a href="#">Visa Code Your Way</a></li>
+      <li><a href="#">Microsoft Codess</a></li>
+      <li><a href="#">Uber She++</a></li>
+      <li><a href="#">Flipkart Girls Wanna Code</a></li>
+      <li><a href="#">LinkedIn CoachIn Program</a></li>
 
-      </ul>
+      </ol>
  
     </div>
   );
