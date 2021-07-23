c2ae026fb4086fa3fe1a424779b63675f0129328
Rishabh <rishkwal@gmail.com>
2021-07-23 13:08:04 +0530

    Added relevant links

--- website/src/pages/events.js
+++ website/src/pages/events.js
@@ -12,24 +12,24 @@ export default function EventsPage() {
      
       <h1 style={{paddingTop:20}}> Events/Programs</h1>
       <ol>
-      <li><a href="#">Google Codejam io for Women</a></li>
-      <li><a href="#">Google STEP Internship</a></li>
-      <li><a href="#">Girlscript Summer of Code</a></li>
-      <li><a href="#">Google Summer of Code</a></li>
-      <li><a href="#">She Codes by Indeed</a></li>
-      <li><a href="#">Outreachy</a></li>
-      <li><a href="#">Goldman Sachs Women Possibilities Summit: Finance</a></li>
-      <li><a href="#">Tech Women at Intuit</a></li>
-      <li><a href="#">Women Who Code: Delhi</a></li>
-      <li><a href="#">Society of Women Engineers India</a></li>
-      <li><a href="#">DevelopHER</a></li>
-      <li><a href="#">LinkedIn Wintathon</a></li>
+      <li><a href="https://codingcompetitions.withgoogle.com/codejamio">Google Codejam io for Women</a></li>
+      <li><a href="https://buildyourfuture.withgoogle.com/programs/step/">Google STEP Internship</a></li>
+      <li><a href="https://gssoc.girlscript.tech/">Girlscript Summer of Code</a></li>
+      <li><a href="https://summerofcode.withgoogle.com/">Google Summer of Code</a></li>
+      <li><a href="https://www.shecodesindeed.com/">She Codes by Indeed</a></li>
+      <li><a href="https://www.outreachy.org/">Outreachy</a></li>
+      <li><a href="https://www.goldmansachs.com/careers/students/programs/india/womens-possibilities-summit-finance.html">Goldman Sachs Women Possibilities Summit: Finance</a></li>
+      <li><a href="https://www.intuit.com/careers/programs/tech-women-at-intuit/">Tech Women at Intuit</a></li>
+      <li><a href="https://www.womenwhocode.com/delhi">Women Who Code: Delhi</a></li>
+      <li><a href="https://india.swe.org/">Society of Women Engineers India</a></li>
+      <li><a href="https://developher.com/">DevelopHER</a></li>
+      <li><a href="https://wintathon2020.splashthat.com/">LinkedIn Wintathon</a></li>
       <li><a href="#">Adobe Codiva</a></li>
       <li><a href="#">Visa Code Your Way</a></li>
-      <li><a href="#">Microsoft Codess</a></li>
-      <li><a href="#">Uber She++</a></li>
+      <li><a href="https://careers.microsoft.com/students/us/en/ind-programs">Microsoft Codess</a></li>
+      <li><a href="https://www.uber.com/us/en/careers/teams/university/">Uber She++</a></li>
       <li><a href="#">Flipkart Girls Wanna Code</a></li>
-      <li><a href="#">LinkedIn CoachIn Program</a></li>
+      <li><a href="https://coachin2021.splashthat.com/">LinkedIn CoachIn Program</a></li>
 
       </ol>
  
