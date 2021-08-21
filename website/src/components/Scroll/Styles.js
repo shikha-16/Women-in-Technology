9578b1aa71fc9ff10bff88b17b328abf7c7b98af
PDahiya123 <pranshudahiyaiitg@gmail.com>
2021-08-21 15:40:43 +0530

    added scroll to top button

---
+++ website/src/components/Scroll/Styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+
+export const Button = styled.div`
+position: fixed;
+width: 3%;
+left: 1%;
+bottom: 80px;
+height: 20px;
+font-size: 3rem;
+z-index: 1;
+cursor: pointer;
+color: light blue;
+`
