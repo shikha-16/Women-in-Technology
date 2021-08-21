07ec159f9963eb484b3bfd668b247948f0cf33f1
Pranshu Dahiya <62893472+PDahiya123@users.noreply.github.com>
2021-08-21 21:51:18 +0530

    Update Styles.js

--- website/src/components/Scroll/Styles.js
+++ website/src/components/Scroll/Styles.js
@@ -3,8 +3,8 @@ import styled from 'styled-components';
 
 export const Button = styled.div`
 position: fixed;
-width: 3%;
-left: 1%;
+width: 15%;
+left: 90%;
 bottom: 80px;
 height: 20px;
 font-size: 3rem;
