2e2875884c9b226ad620a093012a5264ab9eead2
Priyansh Vyas <f20201388@goa.bits-pilani.ac.in>
2021-10-03 01:17:09 +0530

    moved beginners page content to guidelines

--- website/src/components/Header/Header.js
+++ website/src/components/Header/Header.js
@@ -129,9 +129,6 @@ export default function Header() {
                 Contribute
               </a>
               <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                <a className="dropdown-item" href="/beginners">
-                  Beginners
-                </a>
                 <a className="dropdown-item" href="/contributing">
                   Guidelines
                 </a>
