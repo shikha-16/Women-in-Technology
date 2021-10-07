75afcae52d9f906ef5f4ec43778cd490129e57da
Neha Kalbande <neha.leenaharsh@gmail.com>
2021-10-07 14:37:56 +0530

    Update events.js

--- website/src/pages/events.js
+++ website/src/pages/events.js
@@ -1,46 +1,49 @@
-import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import Card from "@material-ui/core/Card";
-import CardActionArea from "@material-ui/core/CardActionArea";
-import CardContent from "@material-ui/core/CardContent";
-import Typography from "@material-ui/core/Typography";
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
 import "./events.css";
-import { eventList } from "../data/eventList.js";
+import {eventList} from "../data/eventList.js"
 
 const useStyles = makeStyles({
-  root: {
-    maxWidth: 345,
-    marginBottom: "8px",
-    marginTop: "8px",
-    justifyContent: "center",
-    textAlign: "center",
-  },
-  media: {
-    height: 140,
-  },
+    root: {
+        maxWidth: 345,
+        marginBottom:'8px',
+        marginTop:"8px",
+        justifyContent:"center",
+        textAlign:"center",
+    },
+    media: {
+        height: 140,
+    },
 });
 
 export default function EventsPage() {
   const classes = useStyles();
 
   return (
-    <>
+     <div>
       <h1 className="eventHeading" style={{ paddingTop: 60 }}>
-        Events/Programs
-      </h1>
-      {eventList.map((a, i) => (
-        <Card className={classes.root}>
-          <CardActionArea>
-            <CardContent>
-              <a href={a.link} target="_blank" rel="noreferrer">
-                <Typography gutterBottom variant="h5" component="h2">
-                  {a.topic}
-                </Typography>
-              </a>
-            </CardContent>
-          </CardActionArea>
-        </Card>
-      ))}
-    </>
-  );
+      Events/Programs
+    </h1>
+{eventList.map((a, i) => (
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardContent>
+        <a href = {a.link} target="_blank">
+          <Typography gutterBottom variant="h5" component="h2">
+           {a.topic}
+          </Typography>
+           </a>
+          
+        </CardContent>
+      </CardActionArea>
+    </Card>
+))}
+
+</div>
+ 
+  )
 }
